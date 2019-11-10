0ac7e2fcc2b57b106ca5e56389a17991cba9da3f
mishkamn <misheel.bayar@ucla.edu>
2019-11-10 15:21:06 -0800

    starting furniture project

---
+++ src/containers/FurniturePlacer/FurniturePlacer.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/Aux/Aux";
+import Furniture from "../../components/Furniture/Furniture";
+
+const FURNITURE_PRICES = {
+    chair: 0.5,
+    ottoman: 0.4,
+    table: 1.3,
+    couch: 0.7
+};
+const FURNITURE_MOVING_PRICES = {
+    chair: 0.5,
+    ottoman: 0.4,
+    table: 1.3,
+    couch: 0.7
+};
+
+class FurniturePlacer extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        furnitures: {
+            chair: 0,
+            ottoman: 0,
+            table: 0,
+            couch: 0
+        },
+        totalPrice: 0,
+        purchasable: false,
+        purchasing: false
+    };
+
+    render() {
+        const disabledInfo = {
+            ...this.state.furnitures
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+        // {salad: true, meat: false, ...}
+        return (
+            <Aux>
+                <p>Modal</p>
+                <Furnitures furnitures={this.state.furnitures} />
+                <p>Build Control</p>
+            </Aux>
+        );
+    }
+}
+
+export default FurniturePlacer;
